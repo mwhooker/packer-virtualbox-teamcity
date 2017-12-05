7a41a99ad38c5f4be4366505b61782d55f091dc2
Matthew Hooker <mwhooker@gmail.com>
2017-12-05 10:17:32 -0800

    packer and bento already exist

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-git clone https://github.com/chef/bento.git
-curl -OL "https://releases.hashicorp.com/packer/1.1.2/packer_1.1.2_linux_amd64.zip"
-unzip packer_1.1.2_linux_amd64.zip
+#git clone https://github.com/chef/bento.git
+#curl -OL "https://releases.hashicorp.com/packer/1.1.2/packer_1.1.2_linux_amd64.zip"
+#unzip packer_1.1.2_linux_amd64.zip
 
 cd bento/ubuntu
-./packer build -only=virtualbox-iso -var "headless=true" ubuntu-16.04-amd64.json 
+/root/packer build -only=virtualbox-iso -var "headless=true" ubuntu-16.04-amd64.json 
